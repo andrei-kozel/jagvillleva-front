5d8da5f0b4326eae26f8a8fdb9fcb3fd054f9aac
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-01-17 19:07:39 +0100

    small fixes

--- components/Help/index.js
+++ components/Help/index.js
@@ -9,12 +9,12 @@ export const Help = () => {
         <div className="w-full px-10 md:px-0 md:w-1/2 relative">
           <Image src="/corgi-tr.png" alt="corgi dog" width={400} height={230} />
         </div>
-        <div className="w-full md:w-1/4 px-16 my-4 md:px-0 justify-center items-center flex flex-col text-center md:text-left">
+        <div className="w-full md:w-1/4 px-16 my-4 md:px-0 justify-center md:justify-start items-center md:items-start flex flex-col text-center md:text-left">
           <p>
             Varje krona som ni kan skänka till hundarna är en chans till att
             förlänga deras liv.
           </p>
-          <div className="relative flex flex-row items-center bg-yellow-300 w-fit px-4 py-2 mb-3 my-5 rounded-xl shadow-md cursor-pointer hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-opacity-75 hover:text-white">
+          <div className="relative flex flex-row items-center bg-yellow-300 w-fit px-4 py-2 mb-2 mt-5 rounded-xl shadow-md cursor-pointer hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-opacity-75 hover:text-white">
             <Image
               src="/icons/swish.png"
               alt="swish logo"
@@ -23,7 +23,7 @@ export const Help = () => {
             />
             <p className="ml-2 font-bold">+46737279717</p>
           </div>
-          <p className="text-sm">
+          <p className="text-sm text-gray-500">
             Snälla skriv era namn, namn på hunden när ni skänker med meddelande
             &quot;gåva till hund&quot; org.
           </p>
